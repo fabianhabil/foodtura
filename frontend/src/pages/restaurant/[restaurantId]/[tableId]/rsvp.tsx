af2cc33e5e615cc2a38e6dbab1225c47e01f4279
fabianhabil <fabianhabilramdhan@gmail.com>
2023-04-04 03:18:00 +0700

    feat: restaurant id table id rsvp page

---
+++ frontend/src/pages/restaurant/[restaurantId]/[tableId]/rsvp.tsx
@@ -0,0 +1,15 @@
+import StoreLayout from '@/components/templates/Store/StoreLayout/StoreLayout';
+
+const Home = () => {
+    return <>ini page rsvp, udah masuk restaurant id dan table id</>;
+};
+
+Home.getLayout = function getLayout(page: React.ReactNode) {
+    return (
+        <>
+            <StoreLayout>{page}</StoreLayout>
+        </>
+    );
+};
+
+export default Home;
