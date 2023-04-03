b88b0c41a08b28b1c25e21e082487ef30c6f6225
fabianhabil <fabianhabilramdhan@gmail.com>
2023-04-04 03:18:41 +0700

    feat: store layout types

---
+++ frontend/src/types/store.d.ts
@@ -0,0 +1,9 @@
+export interface StoreContextType {
+    color: {
+        primary: string;
+        // Secondary -> Text
+        secondary: string;
+        // Third -> Button ?
+        third: string;
+    };
+}
