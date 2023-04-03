36f013b8abd6498877b03ffc011358542db2aa44
fabianhabil <fabianhabilramdhan@gmail.com>
2023-04-04 03:16:53 +0700

    wip: landing page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,26 @@
-import StoreLayout from '@/components/templates/StoreLayout/StoreLayout';
+import { Grid } from '@mui/material';
+import Link from 'next/link';
 
 const Home = () => {
     return (
         <>
-            <StoreLayout>asd</StoreLayout>
+            <Grid
+                container
+                direction='column'
+                sx={{ minHeight: '100vh' }}
+                alignItems='center'
+                justifyContent='center'
+            >
+                <Grid item>
+                    <h2>Landing Page belum jadi</h2>
+                </Grid>
+                <Grid item>
+                    <Link href='/restaurant'>Restaurant Page</Link>
+                </Grid>
+                <Grid item>
+                    <Link href='/admin'>Admin Page</Link>
+                </Grid>
+            </Grid>
         </>
     );
 };
