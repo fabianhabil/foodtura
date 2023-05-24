388203510e25c4ba1afa5a9fec00a78455f769fa
fabianhabil <fabianhabilramdhan@gmail.com>
2023-05-25 01:34:11 +0700

    food category reset query search

--- frontend/src/components/atoms/Store/FoodCategoryButton/FoodCategoryButton.tsx
+++ frontend/src/components/atoms/Store/FoodCategoryButton/FoodCategoryButton.tsx
@@ -1,5 +1,5 @@
-import { Button, Typography } from '@mui/material';
 import type { ColorContextType } from '@/types/store';
+import { Button, Typography } from '@mui/material';
 import type { Dispatch, SetStateAction } from 'react';
 
 const FoodCategoryButton = ({
@@ -7,40 +7,38 @@ const FoodCategoryButton = ({
     index,
     setSelectedCategory,
     title,
-    color
+    color,
+    resetQuery
 }: {
     selectedCategory: number;
     index: number;
     setSelectedCategory: Dispatch<SetStateAction<number>>;
     title: string;
     color: ColorContextType;
+    resetQuery: () => void;
 }) => {
     return (
         <>
             <Button
                 sx={{
-                    backgroundColor:
-                        selectedCategory === index
-                            ? color.secondary
-                            : color.primary,
+                    backgroundColor: selectedCategory === index ? color.secondary : color.primary,
                     '&:hover': {
-                        backgroundColor:
-                            selectedCategory === index
-                                ? color.secondary
-                                : color.primary,
+                        backgroundColor: selectedCategory === index ? color.secondary : color.primary,
                         opacity: 0.8
                     },
                     width: '100%',
                     borderRadius: 0
                 }}
-                onClick={() => setSelectedCategory(() => index)}
+                onClick={() => {
+                    setSelectedCategory(() => index);
+                    resetQuery();
+                }}
             >
                 <Typography
                     sx={{
                         fontSize: '12px',
                         fontWeight: 'bold',
-                        color:
-                            selectedCategory === index ? 'white' : color.third
+                        color: selectedCategory === index ? 'white' : color.third
                     }}
                 >
                     {title}
