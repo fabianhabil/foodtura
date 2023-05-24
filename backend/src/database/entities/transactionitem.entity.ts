6e80672deb9b21daccc93df479ed593de043f1a3
fabianhabil <fabianhabilramdhan@gmail.com>
2023-05-25 01:31:00 +0700

    transaction item entity

---
+++ backend/src/database/entities/transactionitem.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Transaction } from './transaction.entity';
+import { Food } from './food.entity';
+
+@Entity('Transaction_Item')
+export class TransactionItem extends BaseEntity {
+    @PrimaryGeneratedColumn({ name: 'id_transaction_item' })
+    transactionItemId!: number;
+
+    @Column()
+    quantity!: number;
+
+    @Column()
+    price!: number;
+
+    @ManyToOne(() => Transaction)
+    @JoinColumn({ name: 'id_transaction' })
+    transaction!: Transaction;
+
+    @ManyToOne(() => Food)
+    @JoinColumn({ name: 'id_food' })
+    food!: Food;
+}
