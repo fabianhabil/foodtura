fe88b553558444cd14984fc341798275e7edd5e5
fabianhabil <fabianhabilramdhan@gmail.com>
2023-05-25 01:33:02 +0700

    store context updated

--- frontend/src/contexts/StoreContext/StoreContext.tsx
+++ frontend/src/contexts/StoreContext/StoreContext.tsx
@@ -1,16 +1,48 @@
-import type { StoreContextType } from '@/types/store';
-import React, { createContext } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ToastSuccess from '@/components/atoms/Toast/ToastSuccess';
+import type { StoreInfoType, StoreContextType, FoodCartType, FoodType } from '@/types/store';
+import React, { createContext, useEffect, useState } from 'react';
 
 const StoreContext = createContext<StoreContextType | null>(null);
 
 const StoreContextProvider = ({ children }: { children: React.ReactNode }) => {
-    const value = {
-        color: { primary: '#F6DC92', secondary: '#865123', third: '#CC7930' }
+    const [storeInfo, setStoreInfo] = useState<StoreInfoType>({
+        merchantId: '',
+        name: '',
+        address: '',
+        merchantUrl: '',
+        config: {
+            merchantConfigId: '',
+            primaryColor: '#F6DC92',
+            secondaryColor: '#865123',
+            thirdColor: '#CC7930',
+            logoPhotoPath: '/icon/foodtura.png',
+            homePhotoPath: '/cover.png',
+            aboutPhotoPath: '/about.png',
+            aboutDescription: 'Experience the delectable flavors of Italy in every bite at Casetta'
+        }
+    });
+    const [cart, setCart] = useState<FoodCartType[]>([]);
+    const [confirmedCart, setConfirmedCart] = useState<FoodCartType[]>([]);
+    const [openCart, setOpenCart] = useState<boolean>(false);
+
+    const addItemToCart = (food: FoodType, qty: number, notes: string) => {
+        const foodCart: FoodCartType = { ...food, quantity: qty, notes: notes, price: qty * food.price };
+        ToastSuccess(`${foodCart.name} added to cart!`);
+        setCart((state) => [...state, foodCart]);
+        setConfirmedCart((state) => [...state, foodCart]);
     };
 
-    return (
-        <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
-    );
+    const removeItemFromCart = (index: number) => {
+        setCart((state) => state.filter((s, i) => i !== index));
+    };
+
+    useEffect(() => {
+        console.log(cart);
+    }, [cart]);
+
+    const value = { storeInfo, cart, confirmedCart, addItemToCart, openCart, setOpenCart, removeItemFromCart };
+    return <StoreContext.Provider value={value}>{children}</StoreContext.Provider>;
 };
 
 export { StoreContext, StoreContextProvider };
--- frontend/src/types/store.d.ts
+++ frontend/src/types/store.d.ts
@@ -1,7 +1,33 @@
+import type { Dispatch, SetStateAction } from 'react';
+
 export interface StoreContextType {
-    color: ColorContextType;
+    storeInfo: StoreInfoType;
+    cart: FoodCartType[];
+    confirmedCart: FoodCartType[];
+    addItemToCart: (food: FoodType, qty: number, notes: string) => void;
+    openCart: boolean;
+    setOpenCart: Dispatch<SetStateAction<boolean>>;
+    removeItemFromCart: (index: number) => void;
 }
 
+export interface StoreInfoType {
+    merchantId: string;
+    name: string;
+    address: string;
+    merchantUrl: string;
+    config: StoreConfigType;
+}
+
+export interface StoreConfigType {
+    merchantConfigId: string;
+    primaryColor: string;
+    secondaryColor: string;
+    thirdColor: string;
+    logoPhotoPath: string;
+    homePhotoPath: string;
+    aboutPhotoPath: string;
+    aboutDescription: string;
+}
 export interface ColorContextType {
     primary: string;
     // Secondary -> Text
@@ -10,9 +36,17 @@ export interface ColorContextType {
     third: string;
 }
 
-export interface MenuType {
+export interface FoodType {
+    foodId?: number;
     name: string;
     description: string;
     price: number;
     isSpicy?: boolean;
+    isMerchantFavorite?: boolean;
+}
+
+export interface FoodCartType extends FoodType {
+    quantity: number;
+    notes: string;
+    price: number;
 }
