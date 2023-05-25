367b8185c2dde67143473c9a785e4d23716a6302
kenrickpd <kenrickpd@gmail.com>
2023-05-25 12:10:27 +0700

    merchant and merchant create validation

---
+++ backend/src/validations/merchant.validation.ts
@@ -0,0 +1,14 @@
+import { IsString } from "class-validator";
+
+export class MerchantDTO {
+    @IsString()
+    name!: string;
+
+    @IsString()
+    address!: string;
+}
+
+export class MerchantEditDTO extends MerchantDTO {
+    @IsString()
+    merchantUrl!: string;
+}
